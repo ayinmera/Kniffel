edbeb3e18964cb4a033a923d25c34f731610d4b6
ayinmera <humeyra.1931@gmail.com>
2026-05-06 12:33:45 +0200

    safetypoint

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import pygame
+import pygame, player, cup, dice, scorecard
 pygame.init()
-window = pygame.display.set_mode((600,500))
+window = pygame.display.set_mode((1250,700)) #hier wird das Fenster erstellt
+pygame.display.set_caption("Kniffel") #hier wird ein name vergeben für das Fenster
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+    #Erstellung farbe Fenster
+    window.fill((74, 154, 69)) #hier wird die Farbe des Fensters festgelegt
+   
